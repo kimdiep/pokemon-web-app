70fce1904e7548d91abd4df3d3dff61245dad45e
kimdiep <kim.diep.techno@gmail.com>
2019-04-24 21:44:09 +0100

    When request success, iterate through cards JSON and access imageUrlHiRes

--- scripts.js
+++ scripts.js
@@ -4,9 +4,12 @@ request.open('GET', 'https://api.pokemontcg.io/v1/cards?name=mimikyu', true)
 request.onload = function() {
 
   var data = JSON.parse(this.response)
+  var cards = data.cards;
 
   if (request.status >= 200 && request.status < 400) {
-    console.log(data.cards);
+    cards.forEach(card => {
+      console.log(card.imageUrlHiRes);
+    });
   } else {
     console.log('error')
   }
